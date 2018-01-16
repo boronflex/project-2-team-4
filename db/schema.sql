4a4bdf6395e557dd5403a7160b151948c1b4b866
CannonBall <MusicMachine@Mac-c82a1411ae80.local>
2018-01-16 13:04:56 -0600

    schema.sql 4th try

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ USE busapp_db;
 
 CREATE TABLE bus 
 (
-    id INT(20) NOT NULL AUTO_INCREMENT,
-    bus_number INT(20) NOT NULL,
-    bus_driver VARCHAR(100) NOT NULL,
-    riders VARCHAR(100) NOT NULL,
-	capacity INT(20) NOT NULL,
-    home_base VARCHAR(100) NOT NULL,
+    id INT(4) NOT NULL AUTO_INCREMENT,
+    bus_number INT(4) NOT NULL,
+    bus_driver VARCHAR(40) NOT NULL,
+    riders VARCHAR(40) NOT NULL,
+	capacity INT(4) NOT NULL,
+    home_base VARCHAR(40) NOT NULL,
     PRIMARY KEY (id)
     );
     
 CREATE TABLE student
 (
-	id INT(20) NOT NULL AUTO_INCREMENT,
-    bus_number INT(20) NOT NULL,
-    student_last_name VARCHAR(100) NOT NULL,
-    student_first_name VARCHAR(100) NOT NULL,
-    gender VARCHAR(20) NOT NULL,
-    grade_level INT(20) NOT NULL,
-    guardian_name VARCHAR(100) NOT NULL,
-    guardian_email VARCHAR(200) NOT NULL,
-    address_num INT(20) NOT NULL,
-    address_stname VARCHAR(100) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    st VARCHAR(2) NOT NULL,
-	zipcode INT(20) NOT NULL,
-    bus_rider BOOLEAN DEFAULT FALSE
+	id INT(4) NOT NULL AUTO_INCREMENT,
+    bus_number INT(4) NOT NULL,
+    student_last_name VARCHAR(40) NOT NULL,
+    student_first_name VARCHAR(40) NOT NULL,
+    gender VARCHAR(40) NOT NULL,
+    grade_level INT(4) NOT NULL,
+    guardian_name VARCHAR(40) NOT NULL,
+    guardian_email VARCHAR(40) NOT NULL,
+    address_num INT(4) NOT NULL,
+    address_stname VARCHAR(40) NOT NULL,
+    city VARCHAR(40) NOT NULL,
+    st VARCHAR(40) NOT NULL,
+	zipcode INT(4) NOT NULL,
+    bus_rider BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
     );
