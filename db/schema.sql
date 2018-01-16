87280484ca865054fd811e3f61b49527249ec459
CannonBall <MusicMachine@Mac-c82a1411ae80.local>
2018-01-16 10:39:17 -0600

    schema.sql 1st try

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,11 @@ USE busapp_db;
 
 CREATE TABLE bus 
 (
-    id INT(100) NOT NULL AUTO_INCREMENT,
-    bus_number INT(20) NOT NULL,
-    bus_driver VARCHAR() NOT NULL,
-    riders VARCHAR(255) NOT NULL,
-	capacity INT(200) NOT NULL,
+    id INT(4) NOT NULL AUTO_INCREMENT,
+    bus_number INT(4) NOT NULL,
+    bus_driver VARCHAR(100) NOT NULL,
+    riders VARCHAR(100) NOT NULL,
+	capacity INT(100) NOT NULL,
     home_base VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
     );
@@ -16,17 +16,17 @@ CREATE TABLE bus
 CREATE TABLE student
 (
 	id INT(100) NOT NULL AUTO_INCREMENT,
-    bus_number INT(200) NOT NULL,
-    student_last_name VARCHAR(255) NOT NULL,
-    student_first_name VARCHAR(255) NOT NULL,
+    bus_number INT(4) NOT NULL,
+    student_last_name VARCHAR(100) NOT NULL,
+    student_first_name VARCHAR(100) NOT NULL,
     gender VARCHAR(20) NOT NULL,
     grade_level INT(20) NOT NULL,
     guardian_name VARCHAR(100) NOT NULL,
-    guardian_email VARCHAR(255) NOT NULL,
-    address_num INT(10) NOT NULL,
+    guardian_email VARCHAR(200) NOT NULL,
+    address_num INT(20) NOT NULL,
     address_stname VARCHAR(100) NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    st VARCHAR(20) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    st VARCHAR(2) NOT NULL,
 	zipcode INT(10) NOT NULL,
     bus_rider BOOLEAN DEFAULT FALSE
     PRIMARY KEY (id)
