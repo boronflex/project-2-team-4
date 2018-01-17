6d5f649b7a6d8469a6e59d26ec74e8e95221b828
brandongatlin <brandongatlin1981@me.com>
2018-01-17 14:41:56 -0600

    added map to index.handlbars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,48 @@
-var db = require( "../models" );
+var db = require("../models");
+var path = require('path');
+
+var addresses;
+var citiesArr = [];
 
 // Routes
 // =============================================================
-module.exports = function( app ) {
+module.exports = function(app) {
 
   //all routes for students table
   // GET route for getting all of the students
-  app.get( "/api/students", function( req, res ) {
+  app.get("/api/students", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Student.findAll( {} ).then( function( dbStu ) {
-      console.log( "student query happened" );
-      // We have access to the buses as an argument inside of the callback function
-      // res.render( "index" );
-      res.json( dbStu );
-    } );
-  } );
+    db.Student.findAll({}).then(function(dbStu) {
+      console.log("student query happened");
+      // console.log(dbStu);
+
+
+      for (let addresses of Object.values(dbStu)) {
+        cities = addresses.City;
+        citiesArr.push(cities);
+
+      } //end of loop
+
+      console.log("all cities:", citiesArr);
+
+      console.log("first city:", citiesArr[0]);
+
+      // var hbsObject = {
+      //   students: res
+      // };
+      // We have access to the students as an argument inside of the callback function
+      res.json(dbStu);
+
+
+    });
+  });
 
   // POST route for saving a new student
-  app.post( "/api/students", function( req, res ) {
+  app.post("/api/students", function(req, res) {
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
-    db.Student.create( {
+    db.Student.create({
         student_last_name: req.body.student_last_name,
         student_first_name: req.body.student_first_name,
         gender: req.body.gender,
@@ -34,35 +55,35 @@ module.exports = function( app ) {
         Zipcode: req.body.Zipcode,
         Busrider: req.body.Busrider
 
-      } ).then( function( dbStu ) {
-        res.json( dbStu );
-      } )
-      .catch( function( err ) {
+      }).then(function(dbStu) {
+        res.json(dbStu);
+      })
+      .catch(function(err) {
         // Whenever a validation or flag fails, an error is thrown
         // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json( err );
-      } );
-  } );
+        res.json(err);
+      });
+  });
 
   // DELETE route for deleting students. We can get the id of the student to be deleted from
   // req.params.id
-  app.delete( "/api/students/:id", function( req, res ) {
+  app.delete("/api/students/:id", function(req, res) {
     // We just have to specify which student we want to destroy with "where"
-    db.Student.destroy( {
+    db.Student.destroy({
       where: {
         id: req.params.id
       }
-    } ).then( function( dbStu ) {
-      res.json( dbStu );
-    } );
-  } );
+    }).then(function(dbStu) {
+      res.json(dbStu);
+    });
+  });
 
   // PUT route for updating students. We can get the updated student data from req.body
-  app.put( "/api/students", function( req, res ) {
+  app.put("/api/students", function(req, res) {
 
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
-    db.Student.update( {
+    db.Student.update({
         student_last_name: req.body.student_last_name,
         student_first_name: req.body.student_first_name,
         gender: req.body.gender,
@@ -78,70 +99,70 @@ module.exports = function( app ) {
         where: {
           id: req.body.id
         }
-      } ).then( function( dbStu ) {
-        res.json( dbStu );
-      } )
-      .catch( function( err ) {
+      }).then(function(dbStu) {
+        res.json(dbStu);
+      })
+      .catch(function(err) {
         // Whenever a validation or flag fails, an error is thrown
         // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json( err );
-      } );
-  } );
+        res.json(err);
+      });
+  });
 
 
   //all routres for buses table
   // GET route for getting all of the buses
-  app.get( "/api/buses", function( req, res ) {
+  app.get("/api/buses", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Bus.findAll( {} ).then( function( dbBus ) {
-      console.log( "bus query happened" );
+    db.Bus.findAll({}).then(function(dbBus) {
+      console.log("bus query happened");
       // We have access to the buses as an argument inside of the callback function
-      // res.render( "index" );
-      res.json( dbBus );
-    } );
-  } );
+      // res.render("index.handlebars");
+      res.json(dbBus);
+    });
+  });
 
   // POST route for creating a new bus
-  app.post( "/api/buses", function( req, res ) {
+  app.post("/api/buses", function(req, res) {
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
-    db.Bus.create( {
+    db.Bus.create({
         bus_number: req.body.bus_number,
         bus_driver: req.body.bus_driver,
         riders: req.body.riders,
         capacity: req.body.capacity,
         home_base: req.body.home_base
 
-      } ).then( function( dbBus ) {
-        res.json( dbBus );
-      } )
-      .catch( function( err ) {
+      }).then(function(dbBus) {
+        res.json(dbBus);
+      })
+      .catch(function(err) {
         // Whenever a validation or flag fails, an error is thrown
         // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json( err );
-      } );
-  } );
+        res.json(err);
+      });
+  });
 
   // DELETE route for deleting bus routes. We can get the id of the bus to be deleted from
   // req.params.id
-  app.delete( "/api/buses/:id", function( req, res ) {
+  app.delete("/api/buses/:id", function(req, res) {
     // We just have to specify which student we want to destroy with "where"
-    db.Bus.destroy( {
+    db.Bus.destroy({
       where: {
         id: req.params.id
       }
-    } ).then( function( dbBus ) {
-      res.json( dbBus );
-    } );
-  } );
+    }).then(function(dbBus) {
+      res.json(dbBus);
+    });
+  });
 
   // PUT route for updating students. We can get the updated student data from req.body
-  app.put( "/api/buses", function( req, res ) {
+  app.put("/api/buses", function(req, res) {
 
     // Update takes in an object describing the properties we want to update, and
     // we use where to describe which objects we want to update
-    db.Bus.update( {
+    db.Bus.update({
         bus_number: req.body.bus_number,
         bus_driver: req.body.bus_driver,
         riders: req.body.riders,
@@ -151,14 +172,14 @@ module.exports = function( app ) {
         where: {
           id: req.body.id
         }
-      } ).then( function( dbBus ) {
-        res.json( dbBus );
-      } )
-      .catch( function( err ) {
+      }).then(function(dbBus) {
+        res.json(dbBus);
+      })
+      .catch(function(err) {
         // Whenever a validation or flag fails, an error is thrown
         // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-        res.json( err );
-      } );
-  } );
+        res.json(err);
+      });
+  });
 
 }; //end module.exports
--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-const express = require( "express" );
-const methodOverride = require( "method-override" );
-const bodyParser = require( "body-parser" );
-const routes = require( "./controllers/busController.js" )
+const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const routes = require("./controllers/busController.js")
 const app = express();
-const path = require( "path" );
-const db = require( "./models" );
-const exphbs = require( "express-handlebars" );
-const sequelize = require( "sequelize" );
+const path = require("path");
+const db = require("./models");
+const exphbs = require("express-handlebars");
+const sequelize = require("sequelize");
 const PORT = process.env.PORT || 3000;
 
 
 // Sets up the Express app to handle data parsing
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
   extended: true
-} ) );
-app.use( bodyParser.text() );
-app.use( bodyParser.json( {
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
   type: "application/vnd.api+json"
-} ) );
+}));
+
+//testing
+// app.set('views', __dirname + '/views');
+// app.engine('hbs', exphbs({
+//   extname: 'hbs',
+//   defaultLayout: 'main.hbs'
+// }));
+// app.set('view engine', 'hbs');
+// //tesing end
 
 // Static directory
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require( "./routes/api-routes.js" )( app );
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync( {
+db.sequelize.sync({
   // force: true
-} ).then( function() {
-  app.listen( PORT, function() {
-    console.log( "App listening on PORT " + PORT );
-  } );
-} );
+}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
