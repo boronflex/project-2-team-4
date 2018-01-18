a0b614f50473a39497f8a64d8ab62b88c7009362
santokiya <santokiya@gmail.com>
2018-01-17 20:52:51 -0600

    validation on busModel and studentModel

--- models/busModel.js
+++ models/busModel.js
@@ -1,35 +1,42 @@
-var Sequelize = require( "sequelize" );
+var Sequelize = require('sequelize')
 
-module.exports = function( sequelize, DataTypes ) {
+module.exports = function(sequelize, DataTypes) {
+    var Bus = sequelize.define('Bus', {
+        bus_number: {
+            allowNull: false,
+            type: DataTypes.INTEGER(11)
 
-  var Bus = sequelize.define( 'Bus', {
-    bus_number: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 11 ),
+        },
+        bus_driver: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            // validation that checks that this remains between 1 and 50 characters
+            validate: {
+                len: [1, 50]
+            }
 
-    },
-    bus_driver: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
+        },
+        riders: {
+            allowNull: false,
+            type: DataTypes.INTEGER(11)
 
-    },
-    riders: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 11 ),
+        },
+        capacity: {
+            allowNull: false,
+            type: DataTypes.INTEGER(2)
 
-    },
-    capacity: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 2 ),
+        },
+        home_base: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //same validation as above
+            validate: {
+                len: [1, 50]
+            }
 
-    },
-    home_base: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
-
-    }
-  }, {
-    timestamps: false
-  } );
-  return Bus;
-};
+        }
+    }, {
+        timestamps: false
+    })
+    return Bus
+}
--- models/studentModel.js
+++ models/studentModel.js
@@ -1,71 +1,97 @@
-var Sequelize = require( "sequelize" );
+var Sequelize = require("sequelize");
 
-module.exports = function( sequelize, DataTypes ) {
+module.exports = function(sequelize, DataTypes) {
 
-  var Student = sequelize.define( 'Student', {
-    student_last_name: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
+    var Student = sequelize.define('Student', {
+        student_last_name: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //string validation
+            validate: {
+                len: [1, 50]
+            }
+        },
+        student_first_name: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //string validation
+            validate: {
+                len: [1, 50]
+            }
+        },
+        assigned_route: {
+            allowNull: false,
+            type: DataTypes.INTEGER(11),
 
-    },
-    student_first_name: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
+        },
+        gender: {
+            allowNull: false,
+            type: DataTypes.STRING(11),
+            //string validation
+            validate: {
+                len: [1, 11]
+            }
+        },
+        guardian_name: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //string validation
+            validate: {
+                len: [1, 50]
+            }
+        },
+        guardian_email: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //string validation
+            validate: {
+                len: [1, 50]
+            }
+        },
 
-    },
-    assigned_route: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 11 ),
+        address_num: {
+            allowNull: false,
+            type: DataTypes.INTEGER(11),
 
-    },
-    gender: {
-      allowNull: false,
-      type: DataTypes.STRING( 11 ),
+        },
+        address_stname: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //string validation
+            validate: {
+                len: [1, 50]
+            }
+        },
+        City: {
+            allowNull: false,
+            type: DataTypes.STRING(50),
+            //sv
+            validate: {
+                len: [1, 50]
+            }
+        },
+        State: {
+            allowNull: false,
+            type: DataTypes.STRING(20),
+            //sv
+            validate: {
+                len: [1, 20]
+            }
+        },
+        Zipcode: {
+            allowNull: false,
+            type: DataTypes.INTEGER(9),
 
-    },
-    guardian_name: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
+        },
+        Busrider: {
+            allowNull: false,
+            type: DataTypes.BOOLEAN,
+            //set default to false if it isn't supplied with one
+            defaultValue: false
 
-    },
-    guardian_email: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
-
-    },
-
-    address_num: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 11 ),
-
-    },
-    address_stname: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
-
-    },
-    City: {
-      allowNull: false,
-      type: DataTypes.STRING( 50 ),
-
-    },
-    State: {
-      allowNull: false,
-      type: DataTypes.STRING( 20 ),
-
-    },
-    Zipcode: {
-      allowNull: false,
-      type: DataTypes.INTEGER( 9 ),
-
-    },
-    Busrider: {
-      allowNull: false,
-      type: DataTypes.BOOLEAN,
-
-    },
-  }, {
-    timestamps: false
-  } );
-  return Student;
+        },
+    }, {
+        timestamps: false
+    });
+    return Student;
 };
