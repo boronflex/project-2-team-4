1bb1988c85fb5f38ba2c49a1cc7599363c274188
brandongatlin <brandongatlin1981@me.com>
2018-01-19 11:19:34 -0600

    added allowNull true to driver model img col, for now

--- models/drivers.js
+++ models/drivers.js
@@ -2,49 +2,50 @@ var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
 
-    var Driver = sequelize.define('Driver', {
-        
-        driver_first_name: {
-            allowNull: false,
-            type: DataTypes.STRING(50),
-            validate: {
-                len: [1, 50]
-            }
+  var Driver = sequelize.define('Driver', {
 
-        },
+    driver_first_name: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+      validate: {
+        len: [1, 50]
+      }
 
-        driver_last_name: {
-            allowNull: false,
-            type: DataTypes.STRING(50),
-            validate: {
-                len: [1, 50]
-            }
-        },
+    },
 
-        driver_img: {
-            type: DataTypes.BLOB
+    driver_last_name: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+      validate: {
+        len: [1, 50]
+      }
+    },
 
-        },
+    driver_img: {
+      type: DataTypes.BLOB,
+      allowNull: true
 
-        driver_comments: {
-            type: DataTypes.TEXT
+    },
 
-        }
+    driver_comments: {
+      type: DataTypes.TEXT
 
-    }, {
-        timestamps: false
+    }
+
+  }, {
+    timestamps: false
+  });
+
+  Driver.associate = function(models) {
+
+
+    Driver.belongsTo(models.Bus, {
+      // foreignKey: {
+      //   allowNull: false
+      // }
     });
-
-    Driver.associate = function(models) {
-
-
-        Driver.belongsTo(models.Bus, {
-            // foreignKey: {
-            //   allowNull: false
-            // }
-        });
-    };
+  };
 
 
-    return Driver;
+  return Driver;
 };
