ee786ab86acd8d017260ddea71bca248430a04a7
brandongatlin <brandongatlin1981@me.com>
2018-01-19 11:29:32 -0600

    added bus driver seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,8 @@ INSERT INTO students (student_last_name, student_first_name, gender, guardian_na
 INSERT INTO students (student_last_name, student_first_name, gender, guardian_name, guardian_email, address, busrider) VALUES ("Johnson", "Bobby", "Male", "Tammi Johnson", "tj@email.com", "1971 O'Grady Dr Conroe Texas 77304", True);
 
 INSERT INTO students (student_last_name, student_first_name, gender, guardian_name, guardian_email, address, busrider) VALUES ("Washington", "Georgie", "Male", "Marth Washington", "mw@email.com", "24 Dover Dr Conroe, TX 77304", True);
+
+
+
+/* seed data for bus driver */
+INSERT INTO drivers (driver_first_name, driver_last_name, driver_comments) VALUES ('Bussy', 'Gonzalez', 'Drives very spiffily');
