f8aff3246f2ae492bc5e2f9a3504a296d869df44
brandongatlin <brandongatlin1981@me.com>
2018-01-20 23:02:46 -0600

    printing good directions - need styling

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -1,4 +1,6 @@
-var mySteps;
+var mySteps = [];
+var stepsArr = [];
+var steps;
 
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
@@ -39,34 +41,35 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   }, function(response, status) {
     if (status === 'OK') {
 
-      //pulling turn by turn instructions out of response
-      var legsArr = response.routes[0].legs;
+      console.log("response:", response.routes);
+      //
+      // var legsArr = response.routes[0].legs;
+      //
+      // for (var i = 0; i < legsArr.length; i++) {
+      //   // console.log("legsArr:", legsArr);
+      //   stepsArr = legsArr[i].steps;
+      //
+      //   for (var j = 0; j < stepsArr.length; j++) {
+      //
+      //     steps = stepsArr[j].instructions;
+      //
+      //     mySteps.push(steps);
+      //
+      //     for (var h = 0; h < mySteps.length; h++) {
+      //
+      //       var newLi = $("<li>");
+      //       newLi.text(steps);
+      //       $("#steps-list").append(newLi);
+      //
+      //     } // end h loop
+      //   } //end j loop
+      // } // end outter for loop
 
-      for (var i = 0; i < legsArr.length; i++) {
-        // console.log("legsArr:", legsArr);
-        var stepsArr = legsArr[i].steps;
 
-        for (var j = 0; j < stepsArr.length; j++) {
-
-          var steps = stepsArr[j].instructions;
-
-          //trying to push steps into an array of strings so I can export as handlebars object; it say cannot read property of underfined, ie mySteps isn't defined?
-          // mySteps.push(steps);
-          console.log("mySteps:", steps);
-
-          var stepsObj = {
-            steps: mySteps
-          };
-
-          //res is undefined!
-          // res.render("index", steps);
-
-        } //end j loop
-      } // end outter for loop
 
       directionsDisplay.setDirections(response);
       var route = response.routes[0];
-      var summaryPanel = document.getElementById('directions-panel');
+      var summaryPanel = document.getElementById('turn-by-turn');
       summaryPanel.innerHTML = '';
       // For each route, display summary information.
       for (var i = 0; i < route.legs.length; i++) {
@@ -76,9 +79,17 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
         summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
         summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-        // console.log("sum panel", summaryPanel);
 
-      }
+        stepsArr = route.legs[i].steps
+
+        for (var j = 0; j < stepsArr.length; j++) {
+          steps = stepsArr[j].instructions;
+
+          console.log("steps:", steps);
+          $("#steps-list").append(steps);
+        }
+
+      } //end i loop
     } else {
       window.alert('Directions request failed due to ' + status);
     }
