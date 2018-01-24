1147a1369a281aa26a5ae7cd11855c4ac92197ef
CannonBall <MusicMachine@Mac-c82a1411ae80.local>
2018-01-23 20:25:39 -0600

    drag and drop new js file and link to drivers-form.hdlbars

---
+++ public/assets/js/dragandDrop.js
@@ -0,0 +1,25 @@
+
+
+ var fsClient = filestack.init('ArKUq7TzLSaKrbTJi6WB9z');
+ function openPicker() {
+   fsClient.pick({
+     fromSources:["local_file_system","url","googledrive","dropbox"],
+     maxFiles:5
+   }).then(function(response) {
+     // declare this function to handle response
+     //handleFilestack(response);
+     console.log(response);
+     image(response.filesUploaded[0].url)
+   });
+   
+ }
+ openPicker();
+
+function image(driver_img) {
+$.post("/api/image", {driver_img}, function(response, err) {
+ console.log(response);
+})
+}
+
+
+<script src="https://static.filestackapi.com/v3/filestack.js"></script>
